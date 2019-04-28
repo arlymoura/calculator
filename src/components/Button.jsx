3be8cc698190413671bc2a50d9dcba96f9526635
Arly Moura <arlymoura@gmail.com>
2019-04-28 14:11:41 -0300

    basic implement

---
+++ src/components/Button.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../css/Button.css'
+
+export default props =>
+  <button className={`
+    button
+    ${props.operation ? 'operation' : ''}
+    ${props.double ? 'double' : ''}
+    ${props.triple ? 'triple' : ''}
+  `}
+  onClick={e => props.click && props.click(props.label)}
+  >
+    {props.label}
+  </button>
